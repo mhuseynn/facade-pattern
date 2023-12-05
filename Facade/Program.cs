3b27ff44372e8d53bc19def2485b0ec1fa2dc2b8
Mehdiyev Huseyn Penah <Mehdi_nc24@itstep.edu.az>
2023-12-05 21:16:36 +0400

    first

---
+++ Facade/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Facade
+{
+
+    public class Product
+    {
+        public void GetProductDetails()
+        {
+            Console.WriteLine("Product Details");
+        }
+    }
+
+
+    public class Payment
+    {
+        public void MakePayment()
+        {
+            Console.WriteLine("Payment Done Successfully");
+        }
+    }
+
+
+
+    public class Invoice
+    {
+        public void Sendinvoice()
+        {
+            Console.WriteLine("Invoice Send Successfully");
+        }
+    }
+
+
+    public class Order
+    {
+        public void PlaceOrder()
+        {
+            Console.WriteLine("Place Order Started");
+
+            //Get the Product Details
+            Product product = new Product();
+            product.GetProductDetails();
+
+            //Make the Payment
+            Payment payment = new Payment();
+            payment.MakePayment();
+
+            //Send the Invoice
+            Invoice invoice = new Invoice();
+            invoice.Sendinvoice();
+
+            Console.WriteLine("Order Placed Successfully");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order();
+            order.PlaceOrder();
+            Console.Read();
+        }
+    }
+}
